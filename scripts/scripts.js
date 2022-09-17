abc5bd4eae0230e0f268eabc32c9e596c88da15f
bcransom <chanae.ransom@gmail.com>
2022-09-17 13:09:57 -0600

    added script file, made more css changes

---
+++ scripts/scripts.js
@@ -0,0 +1,5 @@
+//Last Updated Date 
+document.getElementById("lastmodified").innerHTML = `<span class="highlight">Last Updated: ${document.lastModified}</span>`;
+
+//Current Year Date
+document.getElementById("currentyear").innerHTML = new Date().getFullYear();
