d6d95778ef8ea3fe5a00fa315f67201c04ce5e07
bcransom <chanae.ransom@gmail.com>
2022-09-24 20:22:01 -0600

    added design page content and script file

---
+++ lesson2/scripts/scripts.js
@@ -0,0 +1,5 @@
+//Last Updated Date 
+document.getElementById("last-modified").innerHTML = `<span class="highlight">Last Updated: ${document.lastModified}</span>`;
+
+//Current Year Date
+document.getElementById("current-year").innerHTML = new Date().getFullYear();
