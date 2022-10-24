d11238ccc548a8f692bbb904daa8d48b08bd553b
bcransom <chanae.ransom@gmail.com>
2022-10-23 23:12:02 -0600

    add lesson7 java activity files and photos

---
+++ lesson7/activity/scripts/script.js
@@ -0,0 +1,41 @@
+//Last Updated Date 
+document.getElementById("last-modified").innerHTML = `<span class="highlight">Last Updated: ${document.lastModified}</span>`;
+
+//Current Year Date
+document.getElementById("current-year").innerHTML = new Date().getFullYear();
+
+// select all data-src attribute
+const imagesToLoad = document.querySelectorAll("img[data-src]");
+
+
+//loads correct image and removes the placeholder
+const loadImages = (image) => {
+    image.setAttribute("src", image.getAttribute("data-src"));
+    image.onload = () => {
+      image.removeAttribute("data-src");
+    };
+  };
+
+//loop through each img and load it
+imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+
+//intersection Observer = load images only when the user scrolls down
+if ("IntersectionObserver" in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if (item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
+    });
+    imagesToLoad.forEach((img) => {
+      observer.observe(img);
+    });
+  } else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+  }
---
+++ lesson7/scripts/windchill.js
@@ -0,0 +1,28 @@
+//get the two input values of temperature and wind-speed,
+//check to make sure they meet the specification limits  
+//(<=50°F and >3.0mph) allowed to officially calculate the wind chill, and
+//either calculate and display the wind chill factor value or 
+//display "N/A" (not applicable) if the input values did not 
+//meet the requirements.
+
+//f = 35.74 + .6215t - 35.75s(0.16power) + 0.4275ts(.16)
+//f = wind chill factor in farenheit 
+//t = air average temperature in farenheit
+//s = wind speed in miles per hour
+
+const temperature = document.querySelector('#temperature');
+const windSpeed = document.querySelector('#wind-speed');
+const windOutput = document.querySelector('#wind-chill');
+
+//let x = parseFloat(temperature.textContent);
+//let y = parseFloat(windSpeed.textContent);
+//console.log(`Temperature is ${x} and Wind Speed is ${y}`);
+
+const windChill = 35.74 + .6215 * temperature - 35.75 * windSpeed ** .16 + .4275 * temperature * windSpeed ** .16;
+
+let result = "N/A";
+if (temperature <=50 && windSpeed > 3.0) {
+    result = windChill;
+}
+
+windOutput.innerHTML = `${result}`;
