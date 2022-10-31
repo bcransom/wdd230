8efbef11917723fb48b1083937deaa78648d65da
bcransom <chanae.ransom@gmail.com>
2022-10-31 16:08:52 -0600

    visit script added

--- lesson7/scripts/script.js
+++ lesson7/scripts/script.js
@@ -40,3 +40,18 @@ if (dateName === 1 || dateName === 2) {
     console.log(dateName);
 };
 
+//local storage dates
+function lastVisit(){
+    let message = "";
+    const currentDate = Date.now();
+    const priorDate = localStorage.getItem("lastVisit");
+    const calculateDate = ((currentDate - priorDate)/86400000).toFixed(0);
+    if (!priorDate){
+        message = "This is your first visit";
+    } else {
+        message = `It has been ${calculateDate} days since last visit`;
+    }
+    localStorage.setItem("lastVisit", currentDate);
+    document.querySelector(".visits").innerHTML = message;
+}
+lastVisit();
