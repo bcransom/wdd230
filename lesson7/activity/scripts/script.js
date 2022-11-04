98ae5c47b922fa175d4fe30ca4de0379465f77cf
bcransom <chanae.ransom@gmail.com>
2022-11-03 20:42:58 -0600

    update lesson7 activity javascript with imgOptions

--- lesson7/activity/scripts/script.js
+++ lesson7/activity/scripts/script.js
@@ -26,6 +26,12 @@ imagesToLoad.forEach((img) => {
   });
 
 //intersection Observer = load images only when the user scrolls down
+
+const imgOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px 300px 0px"
+};
+
 if ("IntersectionObserver" in window) {
     const observer = new IntersectionObserver((items, observer) => {
       items.forEach((item) => {
@@ -34,7 +40,7 @@ if ("IntersectionObserver" in window) {
           observer.unobserve(item.target);
         }
       });
-    });
+    }, imgOptions);
     imagesToLoad.forEach((img) => {
       observer.observe(img);
     });
