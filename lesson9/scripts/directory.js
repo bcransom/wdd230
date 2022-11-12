854b16673e7a81c59ac71d3f3a2824fe62cdd939
bcransom <chanae.ransom@gmail.com>
2022-11-11 18:20:20 -0700

    css directory

--- lesson9/scripts/directory.js
+++ lesson9/scripts/directory.js
@@ -44,3 +44,21 @@ function displayMembers(member) {
 
 let data = getMembers();
 displayMembers(data);
+
+//grid and list button functionality
+//select the grid and list buttons from page
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("article.directory-cards");
+
+gridbutton.addEventListener("click", () => {
+    display.classList.add("grid");
+    display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList);
+
+function showList() {
+    display.classList.add("list");
+    display.classList.remove("grid");
+}
