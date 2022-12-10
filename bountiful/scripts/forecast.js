82c5bdf6af09a768946bec08d557ea7b794c042a
bcransom <chanae.ransom@gmail.com>
2022-12-10 16:51:10 -0700

    comment out forecast file as not working

--- bountiful/scripts/forecast.js
+++ bountiful/scripts/forecast.js
@@ -21,20 +21,28 @@ async function apiFetchForecast() {
   }
 
   function displayResults(forecastData) {
+    // const forecastDate = forecastData[list].dt;
+    // const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
     // const today = new Date();
-    // const day1 = new Date(today);
-    // const day2 = new Date(today);
-    // const day3 = new Date(today);
+    // let day1 = weekday.getDay(forecastDate) + 1;
+    // let day2 = today.getDay() + 2;
+    // let day3 = today.getDay() + 3;
     // day1.setDate(day1.getDate() + 1);
     // day2.setDate(day2.getDate() + 2);
     // day3.setDate(day3.getDate() + 3);
-
+    
     // forecastData.innerHTML = `<strong>${day1.setDate(day1.getDate() + 1)} ${weatherData[1].main.temp.toFixed(2)}</strong>`;
     // forecastDay2.innerHTML = `<strong>${day2.setDate(day2.getDate() + 2)} ${weatherData[2].main.temp.toFixed(2)}</strong>`; 
     // forecastDay3.innerHTML = `<strong>${day3.setDate(day3.getDate() + 3)} ${weatherData[3].main.temp.toFixed(2)}</strong>`;
     // forecastData.innerHTML = `<strong>${weatherData[1].main.temp.toFixed(2)}</strong>`;
 
-    forecast.innerHTML = `<strong>${forecastData[1].main.temp.toFixed(2)}</strong>`;
+    // forecast.innerHTML = `<strong>${day1}: ${forecastData.list[1].main.temp.toFixed(2)}</strong><br>
+    // <strong>${day2}: ${forecastData.list[2].main.temp.toFixed(2)}</strong><br>
+    // <strong>${day3}: ${forecastData.list[3].main.temp.toFixed(2)}</strong>`;
+
+    forecast.innerHTML = `<strong>1 Day Forecast: ${forecastData.list[1].main.temp.toFixed(2)}</strong><br>
+    <strong>2 Day Forecast: ${forecastData.list[2].main.temp.toFixed(2)}</strong><br>
+    <strong>3 Day Forecast: ${forecastData.list[3].main.temp.toFixed(2)}</strong>`;
 
   }
 
