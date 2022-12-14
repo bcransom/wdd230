7ad1d156c31049f2071f8e08ea1b18f71c9f8eea
bcransom <chanae.ransom@gmail.com>
2022-12-13 21:17:13 -0700

    fruit js file attempt

--- bountiful/scripts/fruit.js
+++ bountiful/scripts/fruit.js
@@ -1,14 +1,18 @@
 //fresh form drop down 
 const requestURL = 'https://brotherblazzard.github.io/canvas-content/fruit.json';
 
-let dropdown = document.getElementById('fruit');
-dropdown.length = 0;
+// let dropdown = document.getElementById('fruit');
+// dropdown.length = 0;
 
-let defaultOption = document.createElement('option');
-defaultOption.text = 'Choose fruit...';
+// let defaultOption = document.createElement('option');
+// defaultOption.text = 'Choose fruit...';
 
-dropdown.addEventListener(defaultOption);
-dropdown.selectedIndex = 0;
+// dropdown.addEventListener(defaultOption);
+// dropdown.selectedIndex = 0;
+
+localStorage.setItem("fname", "Chanae");
+
+const cards = document.querySelector('.order-summary');
 
 async function getFruit() {
     const response = await fetch(requestURL);
@@ -16,3 +20,29 @@ async function getFruit() {
     data.fruit.forEach(fruit => {displayFruit(fruit)});
 }
 
+function displayFruit(fruit) {
+    let card = document.createElement('section');
+    let firstName = document.createElement('h3');
+    let email = document.createElement('p');
+    let phone = document.createElement('p');
+    let fruit1 = document.createElement('p');
+    let fruit2 = document.createElement('p');
+    let fruit3 = document.createElement('p');
+    let carb = document.createElement('p');
+
+    card.setAttribute('class', 'card');
+
+    firstName.textContent = `${localStorage.getItem("fname")}`;
+    fruit1.textContent = `${fruit.name}`
+    // carb.textContent = `${fruit.nutritions.carbohydrates}`;
+
+    card.appendChild(firstName);
+    card.appendChild(fruit1);
+    card.appendChild(carb);
+   
+    cards.appendChild(card);
+
+};
+
+let data = getFruit();
+displayFruit(data);
